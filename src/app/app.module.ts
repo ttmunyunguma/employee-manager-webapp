397ef49dfaf2aec26d7e492a9dafd65a2eac340c
Terrence Munyunguma <terence.munyunguma@cassava.co.zw>
2022-01-27 15:09:05 +0200

    [Impl] - changed UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,27 @@ import {AppRoutingModule} from "./app-routing.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EmployeesComponent } from './employees/employees.component';
 import {HttpClientModule} from "@angular/common/http";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { EmployeeEditComponent } from './employees/employee-edit/employee-edit.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    HeaderComponent,
+    FooterComponent,
+    EmployeeEditComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employees/employee.service.ts
+++ src/app/employees/employee.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Employee} from "../models/employee";
-import {Observable} from "rxjs";
+import {Observable, Subject} from "rxjs";
+import {tap} from "rxjs/operators";
 
 const baseUrl = 'http://127.0.0.1:9999/api/v1';
 
@@ -9,11 +10,29 @@ const baseUrl = 'http://127.0.0.1:9999/api/v1';
   providedIn: 'root'
 })
 export class EmployeeService{
+  private employees: Employee[];
+  employeeChanged = new Subject<Employee[]>();
 
   constructor(private http: HttpClient) {
   }
 
-  getAllEmployees(): Observable<any>{
+  setEmployees(employees: Employee[]){
+    this.employees = employees;
+    this.employeeChanged.next(this.employees.slice())
+  }
+
+  getAllEmployees(){
+    return this.employees.slice();
+  }
+  getEmployeeById(id: number): Observable<any>{
     return this.http.get<Employee>(`${baseUrl}/employees/`);
+  }
+
+  fetchEmployees(): Observable<any>{
+    return this.http.get<Employee[]>(`${baseUrl}/employees/`).pipe(
+      tap(response => {
+        this.setEmployees(response);
+      })
+    );
   }
 }
